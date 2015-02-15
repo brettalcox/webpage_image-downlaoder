e6ed5e58f494743b3fdba3521201711111e4d318
brettalcox <brettalcox@gmail.com>
2015-02-14 22:26:21 -0600

    removed curl functions and implemented QNetwork and QUrl objects

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #define CURL_STATICLIB
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <stdio.h>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -21,6 +22,25 @@ size_t writeToFile(void *ptr, size_t size, size_t nmemb, FILE *stream)
     return writtenToFile;
 }
 
+QString urlData;
+
+QString getData() {
+
+    return urlData;
+}
+
+size_t writeCallback(char* buf, size_t size, size_t nmemb, void* up)
+{ //callback must have this declaration
+    //buf is a pointer to the data that curl has for us
+    //size*nmemb is the size of the buffer
+
+    for (int c = 0; c<size*nmemb; c++)
+    {
+        urlData.push_back(buf[c]);
+    }
+    return size*nmemb; //tell curl how many bytes we handled
+}
+
 void MainWindow::on_pushButton_clicked()
 {
     QString threadPath = "/home/brettalcox/Pictures/4chan/Threads/" + ui->lineEdit_3->text();
@@ -28,23 +48,134 @@ void MainWindow::on_pushButton_clicked()
 
     if (!ui->lineEdit->text().isEmpty()) {
         threadUrl = ui->lineEdit->text();
-        char *charThread = new char[threadUrl.length() + 1];
-        strcpy(charThread, threadUrl.toStdString().c_str());
-        downloadFile(charThread, threadPath);
-        delete [] charThread;
     } else {}
 
-    if (!ui->lineEdit_2->text().isEmpty()) {
-        imageUrl = ui->lineEdit_2->text();
-        char *charImage = new char[imageUrl.length() + 1];
-        strcpy(charImage, imageUrl.toStdString().c_str());
-        downloadFile(charImage, imagePath);
-        delete [] charImage;
-    } else {}
+    downloadImageFile(threadUrl);
+
+
+/*
+    QString filePath = threadPath + ui->lineEdit_3->text();
+    QFile outFile("anus");
+    outFile.open(QIODevice::WriteOnly);
+    outFile.write(downloadedData());
+    outFile.close();
+*/
 
 }
 
-void MainWindow::downloadFile(char *url, QString path) {
+void MainWindow::downloadImageFile(QString url) {
+
+
+
+    QNetworkAccessManager *m_WebCtrl = new QNetworkAccessManager;
+    connect(m_WebCtrl, SIGNAL(finished(QNetworkReply*)), SLOT(fileDownloaded(QNetworkReply*)));
+    QUrl theUrl = url;
+    QNetworkRequest request(theUrl);
+    m_WebCtrl->get(request);
+
+}
+
+void MainWindow::fileDownloaded(QNetworkReply *pReply)
+{
+    QByteArray downloadedFile = NULL;
+    downloadedFile = pReply->readAll();
+    emit downloaded();
+
+    QString filePath = threadPath + ui->lineEdit_3->text();
+    QFile outFile(filePath);
+    outFile.open(QIODevice::WriteOnly);
+    outFile.write(downloadedFile);
+    outFile.close();
+
+    pReply->deleteLater();
+
+
+}
+
+QByteArray MainWindow::downloadedData()
+{
+    return *m_DownloadedData;
+}
+
+
+
+
+
+
+
+
+
+
+
+    /*
+
+    CURL *curl;
+    CURLcode res;
+
+    curl = curl_easy_init();
+        if(curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, url);
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
+            //curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
+            curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+            //fclose(file);
+
+        }
+    QString dataFromUrl = getData();
+    char *threadHtml = new char[dataFromUrl.length() + 1];
+    strcpy(threadHtml, dataFromUrl.toStdString().c_str());
+
+    char* imgtag = NULL;
+     for (imgtag = strstr(threadHtml, "<meta ");
+          imgtag != NULL;
+          imgtag = strstr(imgtag+4, "<meta "))
+       {
+         char* srcattr = strstr(imgtag, "src=");
+
+         if (srcattr) {
+                QString theUrl = strtok(srcattr+7, "\"");
+                char *parsedUrl = new char[theUrl.length() + 1];
+                strcpy(parsedUrl, theUrl.toStdString().c_str());
+
+             // parse the src just after srcattr+4
+             // build an URL for the image using snprintf
+             // retreive that image using libcurl
+
+                CURL *curl;
+                CURLcode res;
+                FILE *file;
+                QString name = "doody.jpg";
+                path += name;
+                char *outputFile = new char[path.length() + 1];
+                strcpy(outputFile, path.toStdString().c_str());
+                file = fopen(outputFile, "wb");
+
+                curl = curl_easy_init();
+                    if(curl) {
+                        curl_easy_setopt(curl, CURLOPT_URL, parsedUrl);
+                        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeToFile);
+                        curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);
+                        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+                        res = curl_easy_perform(curl);
+                        curl_easy_cleanup(curl);
+                        fclose(file);
+
+                   }
+                delete [] parsedUrl;
+                delete [] outputFile;
+         }
+        }
+
+     delete [] threadHtml;
+*/
+
+
+
+
+
+void MainWindow::downloadImage(char *url, QString path) {
 
     CURL *curl;
     CURLcode res;
--- mainwindow.h
+++ mainwindow.h
@@ -8,6 +8,13 @@
 #include <QString>
 #include <curl/curl.h>
 
+#include <QObject>
+#include <QByteArray>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkRequest>
+#include <QtNetwork/QNetworkReply>
+#include <QFile>
+
 namespace Ui {
 class MainWindow;
 }
@@ -20,7 +27,12 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     //size_t writeToFile(void *ptr, size_t size, size_t nmemb, FILE *stream);
-    void downloadFile(char*, QString);
+    void downloadImageFile(QString);
+    void downloadImage(char*, QString);
+    QByteArray downloadedData();
+
+signals:
+    void downloaded();
 
     private slots:
         void on_pushButton_clicked();
@@ -28,12 +40,17 @@ public:
         void on_pushButton_2_clicked();
 
         void on_pushButton_3_clicked();
+        void fileDownloaded(QNetworkReply* pReply);
 
 private:
     Ui::MainWindow *ui;
     QString urlName;
     QString threadUrl;
     QString imageUrl;
+
+    QString threadPath = "/home/brettalcox/Pictures/4chan/Threads/";
+    //QNetworkAccessManager m_WebCtrl;
+    QByteArray *m_DownloadedData;
 };
 
 #endif // MAINWINDOW_H
